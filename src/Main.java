81407b7ee7a83b949e73329648e8b9af9c7a3604
jsonyawatkins <76269005+jsonyawatkins@users.noreply.github.com>
2021-04-13 17:18:21 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String openingLyrics="Yesterday, all my troubles seemed so far away";
+        System.out.println(openingLyrics);
+        char expectedGrade='A';
+        System.out.println(expectedGrade);
+        boolean intsCanHoldDecimals = false;
+        System.out.println(intsCanHoldDecimals);
+        double androidShare=81.7;
+        System.out.println(androidShare);
+
+        int numComments=6;
+        //This is where you will print your variable
+        System.out.println(numComments);
+    }
+
+        String name="Jay";
+        int age=30;
+        double desiredSalary=185000.50;
+        char gender='f';
+        boolean lookingForJob=true;
+
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+
+        String firstName = "Samira";
+        String blah = "Smith";
+        String dotCom = "samira@google.com";
+        int salaryexpectation = 100000;
+        int year_of_birth = 1955;
+
+        System.out.println("The program runs!");
+    }
+}
